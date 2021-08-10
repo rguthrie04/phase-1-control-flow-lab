c1f945b11bec57fdc5dc9d3eb6467f921283bd2c
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-10 09:45:43 +0100

    commit tree 7b939ef0de64f6302cb66331ec873e49fb44f99e

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result
+  if (feet<=400) {
+    result = 'This one is on me!'    
+  }
+  else if (feet>2000 && feet<2500) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (feet>=2500){
+    result = `No can do.`
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'?"Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let statement;
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous' :
+      return statement = 'Thank you so much.'
+    case 'not as generous' :
+      return statement = 'Thank you.'
+    default :
+      return statement = 'Bye.'
+  }
 }
